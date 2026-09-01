680d5054c32dba0cc70bd488ec7e3a5c1478b9a2
charlessnydercareer <charles.snyder.career@gmail.com>
2026-09-01 02:29:59 -0400

    build: add Sandbox API compile-time contracts

---
+++ src/Stubs/Sandbox.Common/Contracts.cs
@@ -0,0 +1,50 @@
+namespace Sandbox.ModAPI
+{
+    public static class MyAPIGateway
+    {
+        public static VRage.Game.ModAPI.IMySession? Session { get; }
+    }
+
+    public interface IMyTerminalBlock : Sandbox.ModAPI.Ingame.IMyTerminalBlock, VRage.Game.ModAPI.IMyCubeBlock
+    {
+    }
+
+    public interface IMyFunctionalBlock : IMyTerminalBlock, Sandbox.ModAPI.Ingame.IMyFunctionalBlock
+    {
+    }
+
+    public interface IMyGasGenerator : IMyFunctionalBlock, Sandbox.ModAPI.Ingame.IMyGasGenerator
+    {
+        float ProductionCapacityMultiplier { get; set; }
+        float PowerConsumptionMultiplier { get; set; }
+    }
+
+    public interface IMyCargoContainer : IMyTerminalBlock, Sandbox.ModAPI.Ingame.IMyCargoContainer
+    {
+    }
+}
+
+namespace Sandbox.ModAPI.Ingame
+{
+    public interface IMyTerminalBlock : VRage.Game.ModAPI.Ingame.IMyCubeBlock
+    {
+        string CustomName { get; set; }
+        bool HasLocalPlayerAccess();
+    }
+
+    public interface IMyFunctionalBlock : IMyTerminalBlock
+    {
+        bool Enabled { get; set; }
+    }
+
+    public interface IMyGasGenerator : IMyFunctionalBlock
+    {
+        bool AutoRefill { get; set; }
+        bool UseConveyorSystem { get; set; }
+        bool IsProducing { get; }
+    }
+
+    public interface IMyCargoContainer : IMyTerminalBlock
+    {
+    }
+}
